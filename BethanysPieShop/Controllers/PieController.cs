1f234e5d92a2c6679aff0b95e096c9321728b90c
Александар Арсић <aa223044m@student.etf.bg.ac.rs>
2023-02-16 14:59:45 +0100

    Adding the controller

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,24 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(
+            IPieRepository pieRepository,
+            ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult List()
+        {
+            return View(_pieRepository.AllPies);
+        }
+    }
+}
